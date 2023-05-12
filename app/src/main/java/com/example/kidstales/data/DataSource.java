39452bbf43897834c119f9443a85b66e4ba528a6
DELL <saritta.loloka@gmail.com>
2023-05-12 18:08:44 +0200

    heart icons for favorit  added to the xml

--- app/src/main/java/com/example/kidstales/data/DataSource.java
+++ app/src/main/java/com/example/kidstales/data/DataSource.java
@@ -18,12 +18,12 @@ public class DataSource {
 
     private static void generateStories() {
         stories = new ArrayList<>();
-        stories.add(new Story(R.drawable.story1_cover, "The Ant and The Grasshopper Story"));
+        stories.add(new Story(R.drawable.story1_cover, "The Ant and The Grasshopper"));
         stories.add(new Story(R.drawable.story2_cover, "The Boy Who Cried Wolf"));
         stories.add(new Story(R.drawable.story3_cover, "The Ugly Duckling"));
         stories.add(new Story(R.drawable.story4_cover, "The Hare And The Tortoise"));
-        stories.add(new Story(R.drawable.story5_cover, "The Little Red Riding Hood Short Story"));
-        stories.add(new Story(R.drawable.story6_cover, "The Monkeys and The Cap Seller Story"));
+        stories.add(new Story(R.drawable.story5_cover, "The Little Red Riding Hood"));
+        stories.add(new Story(R.drawable.story6_cover, "The Monkeys and The Cap Seller"));
         stories.add(new Story(R.drawable.story7_cover, "The Golden Egg"));
         stories.add(new Story(R.drawable.story8_cover, "The Cows And The Tiger"));
     }
