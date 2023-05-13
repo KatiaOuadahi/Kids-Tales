70c69ee53058139ce2c52d8fed466d3cf3fc8709
KatiaOuadahi <katiaouadahi16@gmail.com>
2023-05-13 16:19:35 +0200

    Scene Class Added + Story Class Updated

--- app/src/main/java/com/example/kidstales/data/StoriesDataSource.java
+++ app/src/main/java/com/example/kidstales/data/StoriesDataSource.java
@@ -3,15 +3,12 @@ package com.example.kidstales.data;
 import com.example.kidstales.R;
 import com.example.kidstales.model.Scene;
 import com.example.kidstales.model.Story;
-import com.example.kidstales.model.StoryScenes;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class StoriesDataSource {
     private static List<Story> stories;
-
-    private static List<StoryScenes> storiesWithScenes;
-
     private static List <Scene> ScenesOfStory01;
     private static List <Scene> ScenesOfStory02;
 
@@ -23,32 +20,23 @@ public class StoriesDataSource {
     }
 
 
-    public static List<StoryScenes> getStoriesWithScenes() {
-        if (storiesWithScenes == null) {
-            generateStoriesWithScenes();
-        }
-        return storiesWithScenes;
-    }
 
     private static void generateStories() {
-        stories = new ArrayList<>();
-        stories.add(new Story(R.drawable.story1_cover, "The Ant and The Grasshopper"));
-        stories.add(new Story(R.drawable.story2_cover, "The Boy Who Cried Wolf"));
-        stories.add(new Story(R.drawable.story3_cover, "The Ugly Duckling"));
-        stories.add(new Story(R.drawable.story4_cover, "The Hare And The Tortoise"));
-        stories.add(new Story(R.drawable.story5_cover, "The Little Red Riding Hood"));
-        stories.add(new Story(R.drawable.story6_cover, "The Monkeys and The Cap Seller"));
-        stories.add(new Story(R.drawable.story7_cover, "The Golden Egg"));
-        stories.add(new Story(R.drawable.story8_cover, "The Cows And The Tiger"));
-    }
 
-    private static void generateStoriesWithScenes(){
         generateScenesOfStory01();
         generateScenesOfStory02();
-        storiesWithScenes=new ArrayList<>();
-        storiesWithScenes.add(new StoryScenes("First Story",ScenesOfStory01));
-        storiesWithScenes.add(new StoryScenes("Second Story",ScenesOfStory02));
+        stories = new ArrayList<>();
+        stories.add(new Story(R.drawable.story1_cover, "The Ant and The Grasshopper",ScenesOfStory01));
+        stories.add(new Story(R.drawable.story2_cover, "The Boy Who Cried Wolf",ScenesOfStory01));
+        stories.add(new Story(R.drawable.story3_cover, "The Ugly Duckling",ScenesOfStory01));
+        stories.add(new Story(R.drawable.story4_cover, "The Hare And The Tortoise",ScenesOfStory01));
+        stories.add(new Story(R.drawable.story5_cover, "The Little Red Riding Hood",ScenesOfStory01));
+        stories.add(new Story(R.drawable.story6_cover, "The Monkeys and The Cap Seller",ScenesOfStory01));
+        stories.add(new Story(R.drawable.story7_cover, "The Golden Egg",ScenesOfStory01));
+        stories.add(new Story(R.drawable.story8_cover, "The Cows And The Tiger",ScenesOfStory01));
     }
+
+
 
     private static void generateScenesOfStory01(){
         ScenesOfStory01=new ArrayList<>();
--- app/src/main/java/com/example/kidstales/model/Story.java
+++ app/src/main/java/com/example/kidstales/model/Story.java
@@ -1,14 +1,18 @@
 package com.example.kidstales.model;
 
+import java.util.List;
+
 public class Story {
     private int imageResource;
     private String title;
     private boolean isFavorite;
+    private List<Scene> scenes;
 
-    public Story(int imageResource, String title) {
+    public Story(int imageResource, String title ,List<Scene> scenes) {
         this.imageResource = imageResource;
         this.title = title;
         this.isFavorite = false; // Initialize isFavorite to false for all stories
+        this.scenes = scenes;
     }
 
     public int getCoverImage() {
@@ -26,4 +30,5 @@ public class Story {
     public void setFavorite(boolean favorite) {
         isFavorite = favorite;
     }
+    public List<Scene> getScenes() { return scenes; }
 }
