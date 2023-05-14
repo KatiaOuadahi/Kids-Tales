38691872309048b0e9a99a95d0ceafefde0f9bdf
DELL <saritta.loloka@gmail.com>
2023-05-14 22:07:21 +0200

    stories images updated

--- app/src/main/java/com/example/kidstales/data/StoriesDataSource.java
+++ app/src/main/java/com/example/kidstales/data/StoriesDataSource.java
@@ -18,13 +18,13 @@ public class StoriesDataSource {
 
     private static void generateStories() {
         stories = new ArrayList<>();
-        stories.add(new Story(R.drawable.story1_cover, "The Ant and The Grasshopper"));
-        stories.add(new Story(R.drawable.story2_cover, "The Boy Who Cried Wolf"));
-        stories.add(new Story(R.drawable.story3_cover, "The Ugly Duckling"));
-        stories.add(new Story(R.drawable.story4_cover, "The Hare And The Tortoise"));
-        stories.add(new Story(R.drawable.story5_cover, "The Little Red Riding Hood"));
-        stories.add(new Story(R.drawable.story6_cover, "The Monkeys and The Cap Seller"));
-        stories.add(new Story(R.drawable.story7_cover, "The Golden Egg"));
-        stories.add(new Story(R.drawable.story8_cover, "The Cows And The Tiger"));
+        stories.add(new Story(R.drawable.story1_page1, "The Ant and The Grasshopper"));
+        stories.add(new Story(R.drawable.story2_page2, "The Boy Who Cried Wolf"));
+        stories.add(new Story(R.drawable.story3_page4, "The Ugly Duckling"));
+        stories.add(new Story(R.drawable.story4_page3, "The Hare And The Tortoise"));
+        stories.add(new Story(R.drawable.story5_page1, "The Little Red Riding Hood"));
+        stories.add(new Story(R.drawable.story6_page1, "The Monkeys and The Cap Seller"));
+        stories.add(new Story(R.drawable.story7_page2, "The Golden Egg"));
+        stories.add(new Story(R.drawable.story8_page1, "The Cows And The Tiger"));
     }
 }
