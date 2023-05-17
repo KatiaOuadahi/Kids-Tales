d24cd3a1fabe90ac68af3b53f8d9e788643497cf
KatiaOuadahi <katiaouadahi16@gmail.com>
2023-05-17 09:53:52 +0200

    move to the StoriesListActivity when the ib is clicked

--- app/src/main/java/com/example/kidstales/ScenesActivity.java
+++ app/src/main/java/com/example/kidstales/ScenesActivity.java
@@ -3,8 +3,10 @@ package com.example.kidstales;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageButton;
 
 import com.example.kidstales.adapter.ScenesAdapter;
 import com.example.kidstales.adapter.StoryAdapter;
@@ -23,22 +25,26 @@ public class ScenesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scenes);
 
         // Get the Story object passed from StoriesListActivity
-        //Story story = getIntent().getParcelableExtra("Story");
         Gson gson = new Gson();
         String storyJson = getIntent().getStringExtra("Story");
         Story story = gson.fromJson(storyJson, Story.class);
 
-       // Log.d("MY_TAG_activity","THE SELECTED STORY IS "+story.getTitle());
-        Log.d("MY_TAG_activity","THE SIZE OF THE SCENCES "+story.getScenes().size());
-
+        //Get the scenes
         List<Scene> myScenes = story.getScenes();
 
-        Log.d("MY_TAG_activity","THE FIRST SCENE IS "+story.getScenes().get(0).getText());
+
 
         // Set up the ViewPager to display the story's scenes
         ViewPager viewPager = findViewById(R.id.viewPager);
         ScenesAdapter scenesAdapter=new ScenesAdapter(ScenesActivity.this,myScenes);
-          viewPager.setAdapter(scenesAdapter);
-         // viewPager.setCurrentItem(2);
+        viewPager.setAdapter(scenesAdapter);
+
+        //move to the StoriesListActivity when the ib_backToStoriesList is clicked
+        ImageButton backToStoriesList=findViewById(R.id.ib_backToStoriesList);
+        backToStoriesList.setOnClickListener(v->{
+            Intent intent = new Intent(ScenesActivity.this, StoriesListActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
